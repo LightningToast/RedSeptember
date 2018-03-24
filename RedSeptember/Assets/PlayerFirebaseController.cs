19fcf9c8b90e1767e42761c8629d18a59b083fab
Justin Park <jep067@ucsd.edu>
2018-03-24 00:18:51 -0700

    Firebase backing

---
+++ RedSeptember/Assets/PlayerFirebaseController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class PlayerFirebaseController : MonoBehaviour {
+    int counter = 0;
+    DatabaseReference reference;
+
+    public GameObject submarine;
+    SubControl subController;
+	// Use this for initialization
+	void Start () {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://redseptember-5c74c.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        reference.Child("Missions/Match1/Sub").Child("Dive").SetValueAsync(0);
+        reference.Child("Missions/Match1/Sub").Child("Engine").SetValueAsync(0);
+        reference.Child("Missions/Match1/Sub").Child("Pitch").SetValueAsync(0);
+        reference.Child("Missions/Match1/Sub").Child("Torpedo").SetValueAsync(0);
+
+        FirebaseDatabase.DefaultInstance.GetReference("Missions/Match1/Sub/Dive").ValueChanged += HandleDiveChanged;
+        FirebaseDatabase.DefaultInstance.GetReference("Missions/Match1/Sub/Engine").ValueChanged += HandleEngineChanged;
+        FirebaseDatabase.DefaultInstance.GetReference("Missions/Match1/Sub/Pitch").ValueChanged += HandlePitchChanged;
+        FirebaseDatabase.DefaultInstance.GetReference("Missions/Match1/Sub/Torpedo").ValueChanged += HandleTorpedoChanged;
+
+        subController = submarine.GetComponent<SubControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        print("" + args.Snapshot.Child("/Dive"));
+        subController.SetDive(float.Parse(args.Snapshot.Child("/Dive").Value.ToString()));
+        print("" + args.Snapshot.Child("/Engine"));
+        subController.SetEngine(float.Parse(args.Snapshot.Child("/Engine").Value.ToString()));
+        print("" + args.Snapshot.Child("/Pitch"));
+        subController.SetPitch(float.Parse(args.Snapshot.Child("/Pitch").Value.ToString()));
+        print("" + args.Snapshot.Child("/Torpedo"));
+        subController.SetTorpedo();
+    }
+    void HandleDiveChanged(object sender, ValueChangedEventArgs args)
+    {
+        //print("" + args.Snapshot.Child("/Dive"));
+        subController.SetDive(float.Parse(args.Snapshot.Value.ToString()));
+
+    }
+    void HandleEngineChanged(object sender, ValueChangedEventArgs args)
+    {
+        
+        //print("" + args.Snapshot.Child("/Engine"));
+        subController.SetEngine(float.Parse(args.Snapshot.Value.ToString()));
+
+    }
+    void HandlePitchChanged(object sender, ValueChangedEventArgs args)
+    {
+        //print("" + args.Snapshot.Child("/Pitch"));
+        subController.SetPitch(float.Parse(args.Snapshot.Value.ToString()));
+
+    }
+    void HandleTorpedoChanged(object sender, ValueChangedEventArgs args)
+    {
+        
+        //print("" + args.Snapshot.Child("/Torpedo"));
+        subController.SetTorpedo();
+    }
+    public void SendData()
+    {
+        counter++;
+
+        reference.Child("Missions/Match1/Sub").Child("Dive").SetValueAsync(counter);
+    }
+    public void SendDive(float val) {
+        float finVal = val - 0.5f;
+        reference.Child("Missions/Match1/Sub").Child("Dive").SetValueAsync(finVal);
+    }
+    public void SendEngine(float val)
+    {
+        float finVal = val - 0.5f;
+        reference.Child("Missions/Match1/Sub").Child("Engine").SetValueAsync(finVal);
+    }
+    public void SendPitch(float val)
+    {
+        float finVal = val - 0.5f;
+        reference.Child("Missions/Match1/Sub").Child("Pitch").SetValueAsync(finVal);
+    }
+    public void SendTorpedo()
+    {
+        counter++;
+        reference.Child("Missions/Match1/Sub").Child("Torpedo").SetValueAsync(counter);
+    }
+}
---
+++ RedSeptember/Assets/SubControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubControl : MonoBehaviour {
+    public float maxDiveAngle = 45f;
+    public float maxHeight = 3.0f;
+    public float maxEngine = 50f;
+    public int dir = 1;
+    public float targetAngle = 0;
+    Quaternion targetRotation;
+    public float targetHeight = 0;
+    public float targetEngine = 0;
+    public float turnSpeed = 2.0f;
+    public float torpedoSpeed = 20f;
+    public GameObject torpedo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float step = turnSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(targetEngine, targetHeight), 10f * step);
+
+       
+
+	}
+    public void SetDive(float val) {
+        targetHeight = val * maxHeight;
+
+    }
+    public void SetPitch(float val)
+    {
+        targetAngle = val * maxDiveAngle;
+        Vector3 curRot = transform.rotation.eulerAngles;
+        curRot.z = targetAngle;
+        targetRotation = Quaternion.Euler(curRot.x, curRot.y, curRot.z);
+    }
+    public void SetEngine(float val) {
+        targetEngine = val * maxEngine;
+    }
+    public void SetTorpedo() {
+        GameObject tempTorpedo = (GameObject)Instantiate(torpedo, transform.position, transform.rotation);
+        Rigidbody2D tempTorpedoRb = tempTorpedo.GetComponent<Rigidbody2D>();
+        tempTorpedoRb.AddForce(transform.right * torpedoSpeed);
+        Destroy(tempTorpedo, 3f);
+    }
+}
